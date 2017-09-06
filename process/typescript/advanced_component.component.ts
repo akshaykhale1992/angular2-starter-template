ca77fa1a46fa69b32c7888deb7fd2dff09719deb
akshay khale <akshay.khale@vamaship.com>
2017-09-06 13:06:25 +0530

    Updated Advanced Component to support simple Crud

--- process/typescript/advanced_component.component.ts
+++ process/typescript/advanced_component.component.ts
@@ -6,5 +6,42 @@ import {Component} from 'angular2/core';
 })
 
 export class AdvancedComponent {
-	title = "Angular JS 2";
+	branches:any = [];
+
+	edit_branch:boolean = false;
+
+	branch:any = {
+		name    : '',
+		type    : '',
+		pincode : ''
+	};
+	branch_name:string;
+	branch_type:string;
+	branch_pincode:string;
+
+	emptyBranch()
+	{
+		this.branch = [];
+		this.branch.name    = '';
+		this.branch.type    = '';
+		this.branch.pincode = '';
+	}
+
+	addBranch() {
+		this.branches.push(this.branch);
+		this.emptyBranch();
+		this.edit_branch = false;
+	}
+
+	editBranch(index)
+	{
+		if(this.edit_branch === true) {
+			this.addBranch();
+			this.edit_branch = false;
+		} else {
+			this.branch = this.branches[index];
+			this.branches.splice(index, 1);
+			this.edit_branch = true;
+		}
+	}
 }
